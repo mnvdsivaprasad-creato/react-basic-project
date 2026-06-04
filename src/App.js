eb00d7ec46657f7652a89370a591513b3f20db1e
Siva Prasad <mnvdsivaprasad@gmail.com>
2026-06-04 15:17:46 +0530

    Updated App Component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ function App(){
   return(
     <div style={{textAlign: 'center',marginTop:'50px'}}>
       <h1>Welcome Siva</h1>
-        <h2>React Developer Journey</h2>
-        <p>Starts Today </p>
+        <h2>Development branch working</h2>
+    
 
     </div>
   )
