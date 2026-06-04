d6faa0fbe4500cc0ef668d2418ca61c08295551e
Siva Prasad <mnvdsivaprasad@gmail.com>
2026-06-04 11:48:19 +0530

    Initial React App setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App(){
+  return(
+    <div style={{textAlign: 'center',marginTop:'50px'}}>
+      <h1>Welcome Siva</h1>
+        <h2>React Developer Journey</h2>
+        <p>Starts Today </p>
+
+    </div>
+  )
+}
+export default App;
